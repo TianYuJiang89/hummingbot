c3ed72691bbbe2793afb6fb03f46ed7a7f96e326
TianYuJiang <1009485906@qq.com>
2024-01-01 16:52:32 +0800

    test account info

--- scripts/jty_test_acct_info.py
+++ scripts/jty_test_acct_info.py
@@ -1,4 +1,7 @@
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+import pandas as pd
+
 class TestAcountInfo(ScriptStrategyBase):
 
     # gate_io_paper_trade
@@ -24,14 +27,16 @@ class TestAcountInfo(ScriptStrategyBase):
         balance_df = self.get_balance_df()
         if True:
             self.logger().info("balance_df=")
-            self.logger().info(balance_df.to_json(orient="records"))
+            self.logger().info(balance_df)
+            # self.logger().info(balance_df.to_json(orient="records"))
 
 
         #: check active orders
         active_orders_df = self.active_orders_df()
         if True:
             self.logger().info("active_orders_df=")
-            self.logger().info(active_orders_df.to_json(orient="records"))
+            self.logger().info(active_orders_df)
+            # self.logger().info(active_orders_df.to_json(orient="records"))
 
 
         for connector_name, connector in self.connectors.items():
@@ -45,3 +50,26 @@ class TestAcountInfo(ScriptStrategyBase):
                     self.logger().info(f"tick_size= {tick_size}")
                     self.logger().info(f"order_size= {order_size}")
                     self.logger().info(f"min_notional_size= {min_notional_size}")
+
+    def active_orders_df(self) -> pd.DataFrame:
+        """
+        Return a data frame of all active orders for displaying purpose.
+        """
+        columns = ["Exchange", "Market", "Side", "Price", "Amount", "Age"]
+        data = []
+        for connector_name, connector in self.connectors.items():
+            for order in self.get_active_orders(connector_name):
+                age_txt = "n/a" if order.age() <= 0. else pd.Timestamp(order.age(), unit='s').strftime('%H:%M:%S')
+                data.append([
+                    connector_name,
+                    order.trading_pair,
+                    "buy" if order.is_buy else "sell",
+                    float(order.price),
+                    float(order.quantity),
+                    age_txt
+                ])
+        # if not data:
+        #     raise ValueError
+        df = pd.DataFrame(data=data, columns=columns)
+        df.sort_values(by=["Exchange", "Market", "Side"], inplace=True)
+        return df
