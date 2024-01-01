931666e660dcefda817bf2a2d4a543ce41e4d859
TianYuJiang <1009485906@qq.com>
2024-01-01 16:44:25 +0800

    test account info

--- scripts/jty_test_acct_info.py
+++ scripts/jty_test_acct_info.py
@@ -16,19 +16,32 @@ class TestAcountInfo(ScriptStrategyBase):
     }
 
     def on_tick(self):
+
+        if not self.ready_to_trade:
+            return
+
         #: check current balance of coins
-        # balance_df = self.get_balance_df()
-        # self.logger().info(f"Tbalance_df= {balance_df}")
+        balance_df = self.get_balance_df()
+        if True:
+            self.logger().info("balance_df=")
+            self.logger().info(balance_df.to_json(orient="records"))
+
+
+        #: check active orders
+        active_orders_df = self.active_orders_df()
+        if True:
+            self.logger().info("active_orders_df=")
+            self.logger().info(active_orders_df.to_json(orient="records"))
+
 
         for connector_name, connector in self.connectors.items():
             for asset in connector.trading_pairs:
-                self.logger().info(f"asset= {asset}")
-
                 tick_size = connector.get_order_price_quantum(asset, None)
-                self.logger().info(f"tick_size= {tick_size}")
-
                 order_size = connector.get_order_size_quantum(asset, None)
-                self.logger().info(f"order_size= {order_size}")
-
                 min_notional_size = connector.trading_rules[asset].min_notional_size
-                self.logger().info(f"min_notional_size= {min_notional_size}")
+
+                if False:
+                    self.logger().info(f"asset= {asset}")
+                    self.logger().info(f"tick_size= {tick_size}")
+                    self.logger().info(f"order_size= {order_size}")
+                    self.logger().info(f"min_notional_size= {min_notional_size}")
