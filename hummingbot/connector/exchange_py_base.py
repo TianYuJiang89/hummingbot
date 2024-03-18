8c770e4e9ff1a482db2444d788b30ba5f28610c5
TianYuJiang89 <1009485906@qq.com>
2024-03-17 21:00:49 -0700

    acct mng

--- hummingbot/connector/exchange_py_base.py
+++ hummingbot/connector/exchange_py_base.py
@@ -691,11 +691,11 @@ class ExchangePyBase(ExchangeBase, ABC):
         self._stop_network()
         if self.is_trading_required:
             self._trading_rules_polling_task = safe_ensure_future(self._trading_rules_polling_loop())
-            self._trading_fees_polling_task = safe_ensure_future(self._trading_fees_polling_loop())
-            self._status_polling_task = safe_ensure_future(self._status_polling_loop())
-            self._user_stream_tracker_task = self._create_user_stream_tracker_task()
-            self._user_stream_event_listener_task = safe_ensure_future(self._user_stream_event_listener())
-            self._lost_orders_update_task = safe_ensure_future(self._lost_orders_update_polling_loop())
+            # self._trading_fees_polling_task = safe_ensure_future(self._trading_fees_polling_loop())
+            # self._status_polling_task = safe_ensure_future(self._status_polling_loop())
+            # self._user_stream_tracker_task = self._create_user_stream_tracker_task()
+            # self._user_stream_event_listener_task = safe_ensure_future(self._user_stream_event_listener())
+            # self._lost_orders_update_task = safe_ensure_future(self._lost_orders_update_polling_loop())
         else:
             self.order_book_tracker.start()
             self._trading_rules_polling_task = safe_ensure_future(self._trading_rules_polling_loop())
