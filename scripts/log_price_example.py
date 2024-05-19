166aa91092032d2048aaa1df8d52251898e588c9
TianYuJiang <1009485906@qq.com>
2024-05-19 18:51:42 +0800

    add next_funding_utc_timestamp

--- scripts/log_price_example.py
+++ scripts/log_price_example.py
@@ -18,3 +18,13 @@ class LogPricesExample(ScriptStrategyBase):
                 # self.logger().info(f"Best ask: {connector.get_price(asset., True)}")
                 # self.logger().info(f"Best bid: {connector.get_price(asset, False)}")
                 self.logger().info(f"Mid price: {connector.get_mid_price(asset)}")
+
+                if self.is_perpetual(connector_name):
+                    funding_rate = connector.get_funding_info(asset).rate
+                    next_funding_utc_timestamp = connector.get_funding_info(asset).next_funding_utc_timestamp
+                else:
+                    funding_rate = 0.
+                    next_funding_utc_timestamp = None
+
+                self.logger().info(f"funding_rate: {funding_rate}")
+                self.logger().info(f"next_funding_utc_timestamp: {next_funding_utc_timestamp}")
